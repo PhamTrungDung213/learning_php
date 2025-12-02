d5c4f153f0e3beac4c2990f047be6db6a8b966ca
Rymatic6999 <pdung6999@gmail.com>
2025-12-02 23:12:09 +0700

    update 2/12

--- Test.js
+++ Test.js
@@ -1,7 +1,15 @@
-let func=()=>{
+let func1=()=>{
     var ds=["Dung","Anh","Mau","Long","Toan"]
 
     for(var i=0;i<ds.length;i++){
         console.log(ds[i]);
+    }
+    var person={
+        name:"dung",
+        age:20,
+        job:"none"
+    }
+    for(let key in person){
+        console.log(key+":"+person[key]);
     }
 };
